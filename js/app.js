3f122ed9d35a336ac261cd62b815b2c00be629aa
MattDuc <55089890+MattDuc@users.noreply.github.com>
2021-06-14 13:23:14 +0200

    Modifs

--- js/app.js
+++ js/app.js
@@ -9,4 +9,10 @@ app.get('/getImage/:path', function(request, response) {
     response.send('img/neutres/'+req.params.path);
 });
 
+// Ou alors
+
+app.get('/getImages', function(request, response) {
+    response.send(("'img/neutres/chat_neutre1.jpg', 'img/neutres/chat_neutre2.jpg', 'img/neutres/chat_neutre3.jpg', 'img/neutres/chat_neutre4.jpg', 'img/neutres/chat_neutre5.jpg', 'img/neutres/chat_neutre6.jpg', 'img/neutres/chat_neutre7.jpg', 'img/neutres/chat_neutre8.jpg', 'img/neutres/chat_neutre9.jpg','img/neutres/chat_neutre10.jpg', 'img/neutres/chat_neutre11.jpg', 'img/neutres/chat_neutre12.jpg', 'img/neutres/chat_neutre13.jpg'")
+});
+
 server.listen(8080);
